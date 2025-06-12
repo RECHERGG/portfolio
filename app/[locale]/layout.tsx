f186f217d089db956c650da94a6c963b0c0ae712
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-06-12 22:44:03 +0200

    fix error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,25 +1,36 @@
 import { Metadata } from "next";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
+import { hasLocale, Locale, NextIntlClientProvider } from "next-intl";
+import { getMessages, setRequestLocale } from "next-intl/server";
 import { Inter } from "next/font/google";
 import { notFound } from "next/navigation";
 
 import "../globals.css";
+import { locales } from "@/i18n";
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: "jtobaben.me",
     description: "My personal portfolio website",
 };
 
-const inter = Inter({ subsets: ['latin'] });
-
-export default async function LocalLayout({
-    children,
-    params: { locale }
-}: {
+type Props = {
     children: React.ReactNode;
-    params: { locale: string }
-}) {
+    params: Promise<{ locale: Locale }>;
+};
+
+export function generateStaticParams() {
+    return locales.map((locale) => ({ locale }));
+}
+
+export default async function LocalLayout({ children, params }: Props) {
+    const { locale } = await params;
+
+    if (!hasLocale(locales, locale)) {
+        notFound();
+    }
+
+    setRequestLocale(locale);
 
     let messages;
     try {
