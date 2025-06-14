9c4cf6d2b310b4e83ce8fbea5e267f72dfbc1328
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-06-14 14:38:37 +0200

    added animation and some fixes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,41 +1,53 @@
 'use client';
 
+import { motion } from "framer-motion";
 import ProfileCard from '@/components/profile-card';
 import TechStack from '@/components/techstack';
 import { Badge } from '@/components/ui/badge';
 import { IdCard, Languages, MapPin } from 'lucide-react';
 import { useTranslations } from 'next-intl';
+import { Separator } from "@/components/ui/separator";
 
 export default function HomePage() {
     const t = useTranslations();
 
     return (
-        <div className="flex flex-col lg:flex-row gap-6">
-            <aside className="w-full lg:w-[300px]">
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-col lg:flex-row gap-6"
+        >
+            <motion.aside
+                initial={{ x: -50, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                className="w-full lg:w-[300px]"
+            >
                 <ProfileCard />
 
+                <div className="space-y-6">
+                    <div className="flex flex-wrap gap-2 pt-2">
+                        <Badge variant="outline">
+                            <MapPin /> {t("profile.badges.location")}
+                        </Badge>
+                        <Badge variant="outline">
+                            <IdCard /> {Math.floor((Date.now() - new Date("2008-02-10").getTime()) / (365.25 * 24 * 60 * 60 * 1000))} {t("profile.badges.age")}
+                        </Badge>
+                        <Badge variant="outline">
+                            <Languages /> {t("profile.badges.languages.de")}
+                        </Badge>
+                        <Badge variant="outline">
+                            <Languages /> {t("profile.badges.languages.en")}
+                        </Badge>
+                    </div>
 
-                <div className="flex flex-wrap gap-2 pt-2">
-                    <Badge variant="outline">
-                        <MapPin /> {t("profile.badges.location")}
-                    </Badge>
-                    <Badge variant="outline">
-                        <IdCard /> {Math.floor((Date.now() - new Date("2008-02-10").getTime()) / (365.25 * 24 * 60 * 60 * 1000))} {t("profile.badges.age")}
-                    </Badge>
-                    <Badge variant="outline">
-                        <Languages /> {t("profile.badges.languages.de")}
-                    </Badge>
-                    <Badge variant="outline">
-                        <Languages /> {t("profile.badges.languages.en")}
-                    </Badge>
-                </div>
+                    <Separator />
 
-                <div className="pt-12">
                     <h2 className="text-2xl font-bold">TechStack</h2>
-
                     <TechStack />
                 </div>
-            </aside>
+            </motion.aside>
 
             <div className="flex flex-col">
                 {/* Job Position Timeline */}
@@ -56,6 +68,6 @@ export default function HomePage() {
                     {/* Tech stack content here */}
                 </section>
             </div>
-        </div>
+        </motion.div >
     );
 }
--- components/techstack.tsx
+++ components/techstack.tsx
@@ -74,7 +74,7 @@ export default function TechStack() {
             exit={{ opacity: 0, y: -50 }}
             transition={{ duration: 0.5 }}
         >
-            <div className="space-y-6 pt-4 pb-4">
+            <div className="space-y-4 pb-4">
                 {techStack.map((stack) => (
                     <TechStackCard key={`${stack.name}-${stack.type}`} techStack={stack} />
                 ))}
