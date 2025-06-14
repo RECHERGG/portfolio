a7d72d6a23948708e236076769117f57a47338c0
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-06-14 20:56:48 +0200

    updated the footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,8 +1,16 @@
 export default function SiteFooter() {
+    const gitCommitSha = process.env.VERCEL_GIT_COMMIT_SHA;
+    const commitId = gitCommitSha?.substring(0, 7) || "unknown";
+
     return (
         <footer className="py-6 border-t w-[50%]">
             <div className="text-center text-sm text-muted-foreground">
-                © {new Date().getFullYear()} jtobaben.me
+                <p className="text-neutral-400 text-sm">
+                    git <a href={`https://github.com/RECHERGG/portfolio/${gitCommitSha ? `commit/${gitCommitSha}` : ""} `} className="underline" target="_blank" rel="noopener noreferrer">{commitId}</a>
+                </p>
+                <br />
+                Made with 💖 by Joel Tobaben <br />
+                © {new Date().getFullYear()} All rights reserved
             </div>
         </footer>
     );
