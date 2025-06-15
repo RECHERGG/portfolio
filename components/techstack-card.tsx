fa0d9007837fab2e144592fda5d6467cc0a44a41
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-06-15 13:00:45 +0200

    updated hover effect of techstack card

--- components/techstack-card.tsx
+++ components/techstack-card.tsx
@@ -8,7 +8,7 @@ type TechStack = {
 
 export default function TechStackCard({ techStack }: { techStack: TechStack }) {
     return (
-        <div className="dark:bg-neutral-900 bg-neutral-100 rounded-lg flex items-center p-4 gap-4 dark:hover:bg-neutral-700 hover:bg-neutral-200 transition">
+        <div className="dark:bg-neutral-900 bg-neutral-100 rounded-lg flex items-center p-4 gap-4 dark:hover:bg-neutral-700 hover:bg-neutral-200 border hover:border-gray-500 transition select-none">
             <Image src={techStack.src} alt={techStack.name} width={100} height={100} className="w-12 h-12 dark:bg-neutral-800 bg-neutral-300 rounded-md p-2" />
             <div>
                 <p className="text-neutral-900 dark:text-white leading-tight">{techStack.name}</p>
