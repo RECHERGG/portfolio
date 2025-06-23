fff65d61b77ac45e02aaff4f6f67b169e952845e
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-06-23 17:33:29 +0200

    try to fix error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -16,7 +16,6 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
-    metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
     keywords: ["portfolio", "developer", "java", "backend", "tobaben", "joel", "rechergg"],
     authors: [
         {
